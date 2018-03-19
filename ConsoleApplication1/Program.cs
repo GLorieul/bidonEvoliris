b27c7834f317fcb6e745505cca2abb284075b0da
GLorieul <gael.lorieulgl@laposte.net>
2018-03-19 09:54:40 +0100

    Support BBAN formating with '-' between numbers

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -15,16 +15,80 @@ namespace ConsoleApplication1
             code = code * 100 + (ibanCountry[1] - 'A' + 10);
             code *= 100;
             long ibanChecksum = code % 97 - 10;
-            Console.WriteLine($"code=\"{code}\"");
-            Console.WriteLine($"ibanChecksum=\"{ibanChecksum}\"");
+            //Console.WriteLine($"code=\"{code}\"");
+            //Console.WriteLine($"ibanChecksum=\"{ibanChecksum}\"");
             return ibanChecksum;
+        }
+
+        ///Formats iban string for human-friendly display by inserting spaces
+        ///between numbers
+        static string ibanToDisplayString(string iban)
+        {
+            return $"{iban.Substring(0, 4)} " + $"{iban.Substring(4, 4)} "
+                + $"{iban.Substring(8, 4)}  " + $"{iban.Substring(12, 4)}";
         }
 
         static void Main(string[] args)
         {
             Console.Title = "Ma Console Cherie";
 
-            #region QuestionA
+            #region exo01
+            ///Playing with *.Parse() and *.TryParse()
+            //Console.Write("A =");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.Write("B =");
+            //int b;
+            //bool isParse = int.TryParse(Console.ReadLine(), out b);
+
+            //if (isParse)
+            //{
+            //    Console.WriteLine(string.Format($"a + b = {a + b}"));
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Illegal input.");
+            //}
+            #endregion
+
+            #region exo02
+            /// Playing with C# branching instructions
+            //Console.Write("A=");
+            //int nb = int.Parse(Console.ReadLine());
+
+            ////Most naive solution
+            ////bool isEven = ((nb/2 + nb/2) == nb);
+            ////if(isEven)
+            ////{
+            ////    Console.WriteLine("Le nombre est pair");
+            ////}
+            ////else
+            ////{
+            ////    Console.WriteLine("Le nombre est impair");
+            ////}
+
+            //bool isEven = ((nb / 2 + nb / 2) == nb);
+            //string message = isEven ? "Le nombre est pair"
+            //                        : "Le nombre est impair";
+            //Console.WriteLine(message);
+
+            //// Works as well
+            ////bool isEven = ((nb / 2 + nb / 2) == nb);
+            ////switch (isEven)
+            ////{
+            ////    case true:
+            ////        Console.WriteLine("Le nombre est pair");
+            ////        break;
+            ////    case false:
+            ////        Console.WriteLine("Le nombre est impair");
+            ////        break;
+            ////}
+
+            //Console.ReadLine();
+            #endregion
+
+            #region exo02_questionA
+            ///Playing with explicit conversions
             //Console.Write("A=");
             //int nbA = int.Parse(Console.ReadLine());
             //Console.Write("B=");
@@ -35,39 +99,43 @@ namespace ConsoleApplication1
             //Console.WriteLine($"A  %  B = {nbA % nbB}");
             #endregion
 
-            #region QuestionB
+            #region exo02_questionB
+            ///Check that the checksum of a Belgian BBAN account number is correct
+            ///First the checksum is computed from the inputted BBAN
+            ///Then it is compared to the checksum specified in the inputted BBAN
+            /// 
+            ////732-0311849-45
             //Console.Write("BBAN=");
             //string bban = Console.ReadLine();
 
             //Console.WriteLine($"bban =\"{bban}\"");
-            //long bbanNb       = long.Parse(bban.Substring(0,10));
-            //int  bbanChecksum = int.Parse(bban.Substring(10));
+            //long bbanNb = long.Parse(bban.Substring(0, 3) + bban.Substring(4, 7));
+            //long bbanChecksum = long.Parse(bban.Substring(12));
             ////Console.WriteLine($"bbanNb =\"{bbanNb}\"");
             ////Console.WriteLine($"bbanChecksum =\"{bbanChecksum}\"");
             //bool isValidBban = ((bbanNb % 97) == bbanChecksum);
             //string message = isValidBban ? "OK" : "KO";
             //Console.WriteLine(message);
             #endregion
-
-            #region QuestionC
-            /*
+            #region exo02_questionC
+            ///Converts a Belgian BBAN account number into an IBAN account number
+            //Input example: 732-0311849-45
             Console.Write("BBAN=");
             string bban = Console.ReadLine();
             Console.WriteLine($"bban =\"{bban}\"");
 
-            const string ibanCountry = "BE";
-            long bbanNb       = long.Parse(bban.Substring(0, 10));
-            long bbanChecksum = long.Parse(bban.Substring(10));
+            const string IBAN_COUNTRY = "BE";
+            long bbanNb = long.Parse(bban.Substring(0, 3) + bban.Substring(4, 7));
+            long bbanChecksum = long.Parse(bban.Substring(12));
             //Console.WriteLine($"bbanNb =\"{bbanNb}\"");
             //Console.WriteLine($"bbanChecksum =\"{bbanChecksum}\"");
-            
-            long ibanChecksum = computeIbanChecksum(ibanCountry, bbanNb,
+
+            long ibanChecksum = computeIbanChecksum(IBAN_COUNTRY, bbanNb,
                                                     bbanChecksum);
 
-            string iban = ibanCountry + ibanChecksum.ToString()
+            string iban = IBAN_COUNTRY + ibanChecksum.ToString()
                           + bbanNb.ToString() + bbanChecksum.ToString();
-            Console.WriteLine($"IBAN=\"{iban}\"");
-            */
+            Console.WriteLine($"IBAN=\"{ibanToDisplayString(iban)}\"");
             #endregion
 
             Console.ReadLine();
